dde75835c12b606b4d9298e0d48612330665b4d1
Paulo Ferreira <paulo2ferreira@gmail.com>
2025-05-29 08:56:59 -0300

    Renomeando arquivos

--- primeiros_passos/1.introducao.js
+++ primeiros_passos/1.introducao.js
@@ -4,26 +4,12 @@ let nome = 'José';
 var idade = 17;
 const temHobbies = true;
 
-/**
- * apresenta os dados do usuário
- * @param {*} usuarioNome 
- * @param {*} usuarioIdade 
- * @param {*} usuarioTemHobbies 
- * @returns 
- */
 function apresentacaoUsuario(
-    usuarioNome, usuarioIdade, usuarioTemHobbies){
-    
+    usuarioNome, usuarioIdade, usuarioTemHobbies){   
     return (
         'Nome é ' + usuarioNome +
         ', idade é ' + usuarioIdade +
         ' e o usuário tem hobbies: ' + usuarioTemHobbies
     );
 }
-
-/*
-Comentário em bloco
-*/
-
-
 console.log(apresentacaoUsuario(nome, idade, temHobbies));
